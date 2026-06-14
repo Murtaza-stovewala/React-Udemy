3b1d44e3c3e5ce65537562119fd8c302e4af77f6
Murtaza Stovewala <akmurtaza7@gmail.com>
2026-06-14 20:37:05 +0530

    Dynamic & Conditional Styling with CSS Files & CSS Classes

--- 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
+++ 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
@@ -24,13 +24,14 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid':undefined}`}
+          >Email</label>
           <input
             type="email"
-            style={{
-              backgroundColor: emailNotValid ? '#fed2d2':'#d1d5db'
-            }}
-            // className={emailNotValid ? 'invalid' : undefined}
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2':'#d1d5db'
+            // }}
+            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
@@ -38,7 +39,7 @@ export default function AuthInputs() {
           <label>Password</label>
           <input
             type="password"
-            // className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
