1381ef3a563f15d5cd8c6b87c26bfcfe4822a977
Murtaza Stovewala <akmurtaza7@gmail.com>
2026-06-15 00:24:06 +0530

    Scoping CSS Rules with CSS Modules

--- 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
+++ 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
@@ -31,7 +31,7 @@ export default function AuthInputs() {
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2':'#d1d5db'
             // }}
-            className={emailNotValid ? 'invalid' : undefined}
+            className={emailNotValid ? 'invalid' : 'undefined'}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
