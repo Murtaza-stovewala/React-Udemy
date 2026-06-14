3318645845c317ede505d6520e19d40766e6601c
Murtaza Stovewala <akmurtaza7@gmail.com>
2026-06-14 20:37:48 +0530

    forgoted

--- 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
+++ 6 Styling React Components/01-starting-project/src/components/AuthInputs.jsx
@@ -36,7 +36,8 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${passwordNotValid ? 'invalid':undefined}`}
+          >Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
